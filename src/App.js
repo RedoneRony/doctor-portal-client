4f722bb4b150ced0c2fd3231f3a5f0bfec080982
RedoneRony <chokers547@gmail.com>
2021-11-07 15:37:55 +0600

    update private route

--- src/App.js
+++ src/App.js
@@ -2,23 +2,34 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import Appointment from "./pages/Appointment/Appointment/Appointment";
 import Home from "./pages/Home/Home/Home";
-
+import Login from "./pages/Login/Login/Login";
+import AuthProvider from "./contexts/AuthProvider/AuthProvider";
+import Register from "./pages/Login/Register/Register";
+import PrivateRoute from "../src/pages/Login/PrivateRoute/PrivateRoute";
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/appointment">
-            <Appointment />
-          </Route>
-          <Route path="/home">
-            <Home />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
+      <AuthProvider>
+        <Router>
+          <Switch>
+            <PrivateRoute path="/appointment">
+              <Appointment />
+            </PrivateRoute>
+            <Route path="/home">
+              <Home />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
+      </AuthProvider>
     </div>
   );
 }
