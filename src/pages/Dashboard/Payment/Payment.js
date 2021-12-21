666aa988ebf572accc85fd443d6f7c8b803a848d
RedoneRony <chokers547@gmail.com>
2021-12-21 12:11:04 +0600

    update payment process

--- src/pages/Dashboard/Payment/Payment.js
+++ src/pages/Dashboard/Payment/Payment.js
@@ -1,24 +1,45 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm";
+const stripePromise = loadStripe(
+  "pk_test_51K1kqUCz3S5UcSkc6eYD4hAISVZAmT3qsmm5CTG91H4xj01MpHxMnI1wqJ5aPH9MMr2TStn2OErQMCpykJ8xPJkz000KoqogVE"
+);
 
-function Payment() {
+const Payment = () => {
   const { appointmentId } = useParams();
-  const [appointment, setAppintment] = useState();
-  console.log(appointment);
-
+  const [appointment, setAppointment] = useState({});
   useEffect(() => {
     fetch(`http://localhost:5000/appointments/${appointmentId}`)
       .then((res) => res.json())
-      .then((data) => setAppintment(data));
+      .then((data) => setAppointment(data));
   }, [appointmentId]);
   return (
     <div>
       <h2>
-        Please pay for: {appointment.patientName} for {appointment.serviceName}
+        Please Pay for: {appointment.patientName} for {appointment.serviceName}
       </h2>
-      <h4> pay: ${appointment.price}</h4>
+      <h4>Pay: ${appointment.price}</h4>
+      {appointment?.price && (
+        <Elements stripe={stripePromise}>
+          <CheckoutForm appointment={appointment} />
+        </Elements>
+      )}
     </div>
   );
-}
-
+};
 export default Payment;
+
+/*
+1. install stripe and stripe-react
+2. set publishable key
+3. Elements
+4. Checkout Form
+-----
+5. Create payment method
+6. server: create payment Intent api
+7. Load client secret
+8. ConfirmCard payment
+9. handle user error
+*/
